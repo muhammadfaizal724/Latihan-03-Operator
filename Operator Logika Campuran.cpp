60bb6ba258daf9937686bdffacef3f942a8b6aa1
muhammadfaizal724 <55224715+muhammadfaizal724@users.noreply.github.com>
2019-09-19 15:31:15 +0800

    Add files via upload

---
+++ Operator Logika Campuran.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+ int main(){
+ 	
+ 	cout << " Muhammad Faizal " <<  endl;
+	cout << " _______________ " <<  endl<< endl;
+ 	cout << " A or B " <<  endl << endl;
+ 	cout << " 0 || 0 = " << ( 0||0 ) << endl;
+ 	cout << " 0 || 0 = " << ( 0||0 ) << endl;
+ 	cout << " 0 || 1 = " << ( 0||1 ) << endl;
+ 	cout << " 0 || 1 = " << ( 0||1 ) << endl;
+ 	cout << " 1 || 0 = " << ( 1||0 ) << endl;
+ 	cout << " 1 || 0 = " << ( 1||0 ) << endl;
+ 	cout << " 1 || 1 = " << ( 1||1 ) << endl;
+ 	cout << " 1 || 1 = " << ( 1||1 ) << endl << endl;
+ 	
+ 	 	cout << " A or C " <<  endl << endl;
+ 	cout << " 0 || 0 = " << ( 0||0 ) << endl;
+ 	cout << " 0 || 1 = " << ( 0||1 ) << endl;
+ 	cout << " 0 || 0 = " << ( 0||0 ) << endl;
+ 	cout << " 0 || 1 = " << ( 0||1 ) << endl;
+ 	cout << " 1 || 0 = " << ( 1||0 ) << endl;
+ 	cout << " 1 || 1 = " << ( 1||1 ) << endl;
+ 	cout << " 1 || 0 = " << ( 1||0 ) << endl;
+ 	cout << " 1 || 1 = " << ( 1||1 ) << endl << endl;;
+ 	
+ 	cout << " A or B and A or C " <<  endl << endl;
+ 	cout << " 0 || 0 && 0 || 0 = " << ( 0 || 0 && 0 || 0 ) << endl;
+ 	cout << " 0 || 0 && 0 || 1= " << ( 1||0 ) << endl;
+ 	cout << " 0 || 1 && 0 || 0= " << ( 0||0 ) << endl;
+ 	cout << " 0 || 1 && 0 || 1 = " << ( 0||0 ) << endl;
+ 	cout << " 1 || 0 && 1 || 0 = " << ( 0||0 ) << endl;
+ 	cout << " 1 || 0 && 1 || 1 = " << ( 0||0 ) << endl;
+ 	cout << " 1 || 1 && 1 || 0= " << ( 0||0 ) << endl;
+ 	cout << " 1 || 1 && 1 || 1= " << ( 0||1 ) << endl << endl;;
+ 	return 0;
+ }
